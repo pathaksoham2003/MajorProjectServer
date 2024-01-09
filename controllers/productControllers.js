9b85e2e9a2ab0a583a95156637c673e1bbe7bd52
Soham Pathak <soham.pathak@datenwissen.com>
2024-01-09 22:18:06 +0530

    Get Categories for hoe page

--- controllers/productControllers.js
+++ controllers/productControllers.js
@@ -10,6 +10,19 @@ const allProducts = (req,res) => {
     })
 }
 
+const byCategory = (req,res) => {
+    const {category} = req.params;
+    console.log(category);
+    client.query(`SELECT * FROM PRODUCTS_TABLE WHERE category = $1`,[category] , (err,result)=> {
+        if(err){
+            res.status(400).json({message:`Error Quering the database : ${err}`});
+            return;
+        }
+        console.log(result.rows);
+        res.status(200).json(result.rows);
+    })
+}
+
 const specificProduct = (req,res) => {
     const product_id = req.params;
     client.query(`SELECT * FROM PORDUCTS_TABLE WHERE product_id = $1`,[product_id],(err,result)=>{
@@ -39,4 +52,4 @@ const createProduct = async(req,res) => {
       }
 }
 
-module.exports = {allProducts, specificProduct , createProduct};
+module.exports = {allProducts, specificProduct , createProduct , byCategory};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { allProducts , specificProduct , createProduct } = require("../controllers/productControllers");
+const { allProducts , specificProduct , createProduct, byCategory } = require("../controllers/productControllers");
 
 router.get("/all",allProducts);
 router.get("/specific/:id",specificProduct);
-
+router.get("/category/:category",byCategory);
 router.post("/create",createProduct);
 
 module.exports = router;
